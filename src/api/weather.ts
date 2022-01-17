a1c413551451297eb94e5b758ce6293ab831c301
Isaac Miller <miller.isaac96@gmail.com>
2022-01-17 16:00:11 -0500

    retry latlon, return default if both fail

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -70,8 +70,12 @@ const API_KEY = process.env.WEATHER_API_KEY;
 const baseUrl = "https://api.openweathermap.org/data/2.5/onecall";
 
 const once = <T>(fn: () => T): (() => T) => {
-  const res = fn();
-  return () => res;
+  let res: T | undefined;
+  return () => {
+    if (res) return res;
+    res = fn();
+    return res;
+  };
 };
 
 const getLatLon = once(async (): Promise<Coordinates> => {
@@ -83,8 +87,34 @@ const getLatLon = once(async (): Promise<Coordinates> => {
     const { latitude, longitude } = resp.data;
     return { latitude, longitude };
   } catch (err) {
-    console.error(`Error getting IP info: ${err}`);
-    throw err;
+    console.error(
+      `Error getting IP info from ipapi: ${(err as Error).name} ${
+        (err as Error).message
+      }`
+    );
+    try {
+      console.info("Attempting to get IP info from ipinfo");
+      const ipinfoResp = await axios.get<{ loc: string }>(
+        "https://ipapi.co/json",
+        {
+          withCredentials: false,
+        }
+      );
+      const { loc } = ipinfoResp.data;
+      const [latitude, longitude] = loc.split(",").map(Number);
+      return { latitude, longitude };
+    } catch (err2) {
+      console.error(
+        `Error getting IP info from ipinfo:  ${(err2 as Error).name} ${
+          (err2 as Error).message
+        }`
+      );
+      console.warn(`Returning default coordinates (DC)...`);
+      return {
+        latitude: 38.9982,
+        longitude: -77.0338,
+      };
+    }
   }
 });
 
